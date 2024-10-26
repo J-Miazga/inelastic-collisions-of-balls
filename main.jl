951a190386726d75fea06be86585327f97b04c6d
J-Miazga <kuba.miazga@wp.pl>
2024-10-26 23:16:20 +0200

    Interface finished

--- main.jl
+++ main.jl
@@ -4,12 +4,7 @@
 # using OrdinaryDiffEq
 
 # Function to generate the coordinates for a circle
-# function generate_circle_points(center, radius, n_points = 100)
-#     angles = LinRange(0, 2pi, n_points)  # Create angle steps
-#     xs = center[1] .+ radius .* cos.(angles)  # X coordinates
-#     ys = center[2] .+ radius .* sin.(angles)  # Y coordinates
-#     return xs, ys
-# end
+
 # mutable struct Ball
 #     position::Vector{Float64}
 #     force::Vector{Float64}
@@ -17,58 +12,139 @@
 #     radius::Float64
 # end
 
+# using GLMakie
+# using Observables
+# using GeometryBasics
+
+# mutable struct Ball
+#     mass::Float32
+#     position::Vector{Float32}
+#     force::Vector{Float32}
+# end
+# function generate_circle_points(center, radius::Float64, n_points = 100)
+#     angles = LinRange(0, 2pi, n_points)  # Create angle steps
+#     xs = center[1] .+ radius .* cos.(angles)  # X coordinates
+#     ys = center[2] .+ radius .* sin.(angles)  # Y coordinates
+#     return xs, ys
+# end
+# balls=Vector{Ball}()
+# # Create a figure and axis
+# fig = Figure(size=(1920, 1080))
+# ax = Axis(fig[1, 1],aspect=DataAspect())
+# Makie.deactivate_interaction!(ax,:rectanglezoom)
+
+# # Create a line plot
+
+# lines!(ax,Circle(Point2f0(1.0,1.0), 1.0), color = :black, linewidth = 10)
+# buttons_grid= fig[2,1]= GridLayout(tellwidth=false)
+# return mass_box = Textbox(buttons_grid[1,1],placeholder="Mass", height=30,width=150)
+# force_box = Textbox(buttons_grid[1,2],placeholder="Force",height=30,width=150)
+# button1 = Button(buttons_grid[1,3], label="Submit",height=30,width=150)
+# Label(buttons_grid[1, 4], "Run Simulation")
+# toggle=Toggle(buttons_grid[1,5],height=30,width=50)
+
+# spoint =select_point(ax.scene)
+# control=1
+# position_of_center=Vector{Float32}()
+# alfa=0
+# on(spoint) do position
+#     if control%2==1
+#         x,y=generate_circle_points(position, 0.1, 100)
+#         poly!(ax,x,y, color = :black)
+#         println("Position x= $(position[1]) and y= $(position[2])")
+#         global position_of_center=position
+#         global control+=1
+#     else
+#         global alfa=atand(abs(position[2]-position_of_center[2])/abs(position[1]-position_of_center[1]))
+#     end
+# end 
+# on(button1.clicks) do g
+#     if !isnothing(mass_box.stored_string[]) && !isnothing(force_box.stored_string[])
+#         vector_force=Vector{Float32}()
+#         mass = tryparse(Float32, mass_box.stored_string[])
+#         force = tryparse(Float32, force_box.stored_string[])
+#         vector_force=[force*cos(alfa),force*sin(alfa)]
+#         new_ball=Ball(mass,position_of_center,vector_force)
+#         push!(balls,new_ball)
+#         global control+=1
+#         mass_box.stored_string=nothing
+#         force_box.stored_string=nothing
+#     end
+# end
+
+# # Display the plot
+# display(fig)
 using GLMakie
 using Observables
 using GeometryBasics
 
-# Create a figure and axis
-fig = Figure(size=(1920, 1080))
-ax = Axis(fig[1, 1],aspect=DataAspect())
-Makie.deactivate_interaction!(ax,:rectanglezoom)
-# Create a line plot
-
-lines!(ax,Circle(Point2f0(1.0,1.0), 1.0), color = :black, linewidth = 10)
-number_of_balls=Observable(0)
-# Adding a textbox for number of balls
-text_box = Textbox(fig[2,1],placeholder="Number of balls", tellwidth = false)
-
-# Create a button to confirm the input
-button1 = Button(fig[2,2], label="Submit")
-button2 = Button(fig[2,3], label="Run")
-
-on(button1.clicks) do n
-    println("Button clicked")
-    number_of_balls[] = tryparse(Int, text_box.stored_string[])
-    println("Number of balls set to: $(number_of_balls[])")  # Show valid integer   
+mutable struct Ball
+    mass::Float32
+    position::Vector{Float32}
+    force::Vector{Float32}
 end
-ball_mat=Matrix{Float32}(undef,0,2)
-spoint =select_point(ax.scene)
-on(spoint) do z
-    if number_of_balls[]!=size(ball_mat,1)
-    global ball_mat
-    position=[0.0,0.0]
-    position=z
-    ball_mat=vcat(ball_mat,position')
-    lines!(ax,Circle(Point2f0(ball_mat[size(ball_mat,1),1],ball_mat[size(ball_mat,1),2]), 0.1), color = :black,linewidth = 2)
-    println("Position x= $(position[1]) and y= $(position[2])")
+mutable struct Balls_State
+    control::Int
+    position_of_center::Vector{Float32}
+    alfa::Float32
+end
+function init_state()
+    init=Balls_State(1,[0.0,0.0],0.0)
+end
+function generate_circle(center, radius::Float64, n_points = 100)
+    angles = LinRange(0, 2pi, n_points) 
+    xs = center[1] .+ radius .* cos.(angles)  
+    ys = center[2] .+ radius .* sin.(angles)  
+    return xs, ys
+end
+function click_plot_check(state::Balls_State, ax, position)
+    if state.control % 2 == 1
+        x, y = generate_circle(position, 0.1, 100)
+        poly!(ax, x, y, color=:black)
+        state.position_of_center = position  
+        state.control += 1                 
+    else
+        state.alfa = atand(abs(position[2] - state.position_of_center[2]) / abs(position[1] - state.position_of_center[1]))
+    end
+end
+function button_click_check(state::Balls_State, balls, mass_box, force_box)
+    if !isnothing(mass_box.stored_string[]) && !isnothing(force_box.stored_string[])
+        mass = tryparse(Float32, mass_box.stored_string[])
+        force = tryparse(Float32, force_box.stored_string[])
+        vector_force = [force * cos(state.alfa), force * sin(state.alfa)]       
+        new_ball = Ball(mass, state.position_of_center, vector_force)
+        push!(balls, new_ball)
+
+        state.control += 1
+        mass_box.stored_string= nothing
+        force_box.stored_string= nothing
     end
 end
 
-# Display the plot
+create_state = init_state()
+fig = Figure(size=(1920, 1080))
+ax = Axis(fig[1, 1],aspect=DataAspect())
+Makie.deactivate_interaction!(ax,:rectanglezoom)
+
+lines!(ax,Circle(Point2f0(1.0,1.0), 1.0), color = :black, linewidth = 10)
+buttons_grid= fig[2,1]= GridLayout(tellwidth=false)
+mass_box = Textbox(buttons_grid[1,1],placeholder="Mass",reset_on_defocus=true, height=30,width=150)
+force_box = Textbox(buttons_grid[1,2],placeholder="Force",reset_on_defocus=true,height=30,width=150)
+button1 = Button(buttons_grid[1,3], label="Submit",height=30,width=150)
+Label(buttons_grid[1, 4], "Run Simulation")
+toggle=Toggle(buttons_grid[1,5],height=30,width=50)
+spoint = select_point(ax.scene)
+balls=Vector{Ball}()
+
+on(spoint) do position
+    click_plot_check(create_state, ax, position)
+end
+
+on(button1.clicks) do g
+    button_click_check(create_state, balls, mass_box, force_box)
+end
+
 display(fig)
- 
-# fig = Figure(size = (1920, 1080))
-# ax = Axis(fig[1, 1], aspect= DataAspect())
-# Makie.deactivate_interaction!(ax,:rectanglezoom)
-# spoint =select_point(ax.scene)
-
-
-# on(spoint) do z
-#     position_x,positon_y=z
-#     println(position_x)
-# end
-
-# display(fig)
 
 
 
