1c38394c61a1873db15d265e319419bdb65d4bc6
J-Miazga <kuba.miazga@wp.pl>
2024-11-09 17:27:06 +0100

    Added few balls movement without collisions

--- main.jl
+++ main.jl
@@ -14,6 +14,16 @@ mutable struct Balls_State
     position_of_center::Vector{Float32}
     alfa::Float32
 end
+# mutable struct state
+#     p_x::Float32
+#     p_y::Float32
+#     v_x::Float32
+#     v_y::Float32
+# end
+# mutable struct input
+#     f_x::Float32
+#     f_y::Float32
+# end
 function init_state()
     init=Balls_State(1,[0.0,0.0],0.0)
 end
@@ -23,11 +33,11 @@ function generate_circle(center::Vector{Float32}, radius::Float64, n_points::Int
     ys = center[2] .+ radius .* sin.(angles) 
     return xs, ys
 end
-function click_plot_check(state::Balls_State, position)
+function click_plot_check(ball_state::Balls_State, position)
     if((position[1]^2+position[2]^2)>0.8)
         return
     end
-    if state.control % 2 == 1
+    if ball_state.control % 2 == 1
         position1 = Observable(Vector{Float32}(position))
         push!(center_vec, position1)
         new_circle = @lift generate_circle($position1, 0.1, 30)
@@ -35,24 +45,22 @@ function click_plot_check(state::Balls_State, position)
         push!(xs, @lift $new_circle[1])
         push!(ys, @lift $new_circle[2])
         poly!(ax, xs[end], ys[end]; color=:black) 
-        state.position_of_center = position  
-        state.control += 1                 
+        ball_state.position_of_center = position  
+        ball_state.control += 1                 
     else
-        state.alfa = atan(position[2] - state.position_of_center[2] , position[1] - state.position_of_center[1])
+        ball_state.alfa = atan(position[2] - ball_state.position_of_center[2] , position[1] - ball_state.position_of_center[1])
     end
 end
-function button_click_check(state::Balls_State, balls, mass_box, force_box)
+function button_click_check(ball_state::Balls_State, balls, mass_box, force_box)
     if !isnothing(mass_box.stored_string[]) && !isnothing(force_box.stored_string[])
         mass = tryparse(Float32, mass_box.stored_string[])
         force = tryparse(Float32, force_box.stored_string[])
-        vector_force = [force * cos(state.alfa), force * sin(state.alfa)]       
-        new_ball = Ball(mass, state.position_of_center, vector_force)
+        vector_force = [force * cos(ball_state.alfa), force * sin(ball_state.alfa)]       
+        new_ball = Ball(mass, ball_state.position_of_center, vector_force)
         push!(balls, new_ball)
-        push!(state_vec,state.position_of_center[1])
-        push!(state_vec,state.position_of_center[2])
-        push!(state_vec,vector_force[1]/mass)
-        push!(state_vec,vector_force[2]/mass)
-        state.control += 1
+        push!(state_vec,[ball_state.position_of_center[1],ball_state.position_of_center[2],vector_force[1]/mass,vector_force[2]/mass])
+        push!(input_vec,[0.0f0,0.0f0])
+        ball_state.control += 1
         mass_box.stored_string= nothing
         force_box.stored_string= nothing
     end
@@ -78,6 +86,15 @@ function check_collison(ball::Ball)
         return true
     else 
         return false
+    end
+end
+function check_collision_balls(ball:Ball,j::int)
+    for each in 2:length(ball)
+        if(sqrt((ball[j].position[1]-ball[each].position[1])^2+(ball[j].position[2]-ball[each].position[2])^2)<=0.2)
+            return true
+        else 
+            return false
+        end
     end
 end
 center_vec=Vector{Observable{Vector{Float32}}}()
@@ -127,11 +144,62 @@ end
 global no_bounces=false
 Matrix_A = zeros(Float32, 4, 4)  # 4x4 matrix of zeros
 Matrix_B = zeros(Float32, 4, 2)  # 4x2 matrix of zeros
+const friction_coefficient=0.2f0
 const g=9.81f0
 const step=0.005f0
-state_vec=Vector{Float32}()
-new_state_vec=Vector{Float32}([0.0f0,0.0f0,0.0f0,0.0f0])
-input=Vector{Float32}([0.0f0,0.0f0])
+state_vec=Vector{Vector{Float32}}()
+#new_state_vec=Vector{State}()
+input_vec=Vector{Vector{Float32}}()
+if toggle.active[]
+    for i in 1:1000
+        for j in 1:length(balls)
+            if i==1
+                input_vec[j].=[balls[j].force[1],balls[j].force[2]-g*balls[j].mass]
+            # elseif no_bounces
+            #     force_normal=balls[1].mass*g/cos(atan(state_vec[2],state_vec[1]))
+            #     input.=[g*balls[1].mass*sin(atan(state_vec[2]/state_vec[1])),-g*balls[1].mass]
+            #     #input.=[force_normal*cos(atan(state_vec[2],state_vec[1]))*(1-friction_coefficient),-g*balls[1].mass+force_normal*sin(atan(state_vec[2],state_vec[1]))*(1-friction_coefficient)]
+            else
+                input_vec[j].=[0.0f0,-g*balls[j].mass]
+            end  
+
+            state_vec[j].=DiffEq(state_vec[j],input_vec[j],balls[j])
+
+            if i%2==0 && check_collison(balls[j]) && no_bounces 
+                state_vec[j][2]-=(state_vec[j][2]+sqrt(0.81-state_vec[j][1]^2))
+                center_vec[j][] = [state_vec[j][1],state_vec[j][2]]
+            elseif i%2==0
+                center_vec[j][] = [state_vec[j][1],state_vec[j][2]]     
+            end  
+            
+            balls[j].position=[state_vec[j][1],state_vec[j][2]]
+            #println("Speed  Vx=$(state_vec[3])  Vy=$(state_vec[4])")   
+            if check_collison(balls[j])
+                if no_bounces
+                 continue
+                end
+                if abs(state_vec[j][3]) <0.1 && abs(state_vec[j][4]) <0.1 && !no_bounces && state_vec[j][2]<0
+                    println("no bounces")
+                    global no_bounces=true
+                end  
+                println("Zderzenie!!!!!")
+                dist = sqrt(balls[j].position[1]^2+balls[j].position[2]^2)
+                normal_x = balls[j].position[1] / dist
+                normal_y = balls[j].position[2] / dist 
+                v_normal = state_vec[j][3] * normal_x + state_vec[j][4] * normal_y
+                if v_normal > 0
+                    state_vec[j][3] =  (state_vec[j][3]-(2) * v_normal * normal_x)*e[] 
+                    state_vec[j][4] =  (state_vec[j][4]-(2) * v_normal * normal_y)*e[]
+                end   
+            end
+            if check_collision_balls(balls,j)
+
+            end
+            
+        end
+        sleep(step)
+    end
+end
 # @async begin 
 #     while true
 #         if !isopen(fig.scene) 
@@ -148,47 +216,6 @@ input=Vector{Float32}([0.0f0,0.0f0])
 #         sleep(0.01)
 #     end
 # end
-for i in 1:1000
-    if i==1
-        input.=[balls[1].force[1],balls[1].force[2]-g*balls[1].mass]     
-    # elseif no_bounces
-    #     input.=[g*balls[1].mass*sin(atan(-state_vec[2]/-state_vec[1])),-g*balls[1].mass]
-    else
-        input.=[0.0f0,-g*balls[1].mass]
-    end   
-    new_state_vec.=DiffEq(state_vec,input,balls[1])
-    state_vec.=new_state_vec
-    if i%2==0 && check_collison(balls[1]) && no_bounces 
-        state_vec[2]-=(state_vec[2]+sqrt(0.81-state_vec[1]^2))
-        center_vec[1][] = [state_vec[1],state_vec[2]]
-        balls[1].position=[state_vec[1],state_vec[2]]
-    elseif i%2==0
-        center_vec[1][] = [state_vec[1],state_vec[2]]
-    end
-    balls[1].position=[state_vec[1],state_vec[2]]
-    println("Speed  Vx=$(state_vec[3])  Vy=$(state_vec[4])")   
-    if check_collison(balls[1])
-        if abs(state_vec[3]) <0.1 && abs(state_vec[4]) <0.1 && !no_bounces && state_vec[2]<0
-            println("no bounces")
-            global no_bounces=true
-            # state_vec[3]=0.0f0
-            # state_vec[4]=0.0f0
-        end
-        if no_bounces
-            continue
-        end
-        println("Zderzenie!!!!!")
-        dist = sqrt(balls[1].position[1]^2+balls[1].position[2]^2)
-        normal_x = balls[1].position[1] / dist
-        normal_y = balls[1].position[2] / dist 
-        v_normal = state_vec[3] * normal_x + state_vec[4] * normal_y
-        if v_normal > 0
-            state_vec[3] =  (state_vec[3]-(2) * v_normal * normal_x)*e[] 
-            state_vec[4] =  (state_vec[4]-(2) * v_normal * normal_y)*e[]
-        end   
-    end
-    sleep(step)
-end
 # on(toggle.active) do y
 #     @async while toggle.active[]
 #         if !isopen(fig.scene) 
